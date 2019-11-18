a4b0e13a5376fcabb875b84b350eeae4b5d60f40
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-11-18 21:33:57 +0700

    Fix delete post

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ class PostResource(Resource):
 
     def delete(self, post_id):
         post = Post.query.get_or_404(post_id)
-        post.delete()
+        db.session.delete(post)
         db.session.commit()
         return '', 204
 
