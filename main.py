5017ce784bde1a289cefa7a3a6ea70e803675b85
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-11-25 19:34:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,74 +1,62 @@
 from flask import Flask, request
+from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from flask_restful import Api, Resource
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
-api = Api(app)
-
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(50))
-    content = db.Column(db.String(255))
-
-    def __repr__(self):
-        return '<Post %s>' % self.title
-
+    title = db.Column(db.String(100))
+    content = db.Column(db.Text)
 
 class PostSchema(ma.Schema):
     class Meta:
-        fields = ("id", "title", "content")
-
+        fields = ('id', 'title', 'content')
 
 post_schema = PostSchema()
 posts_schema = PostSchema(many=True)
 
-
-class PostListResource(Resource):
+class PostsResource(Resource):
     def get(self):
-        posts = Post.query.all()
-        return posts_schema.dump(posts)
-
+        return posts_schema.dump(Post.query.all())
+    
     def post(self):
-        new_post = Post(
-            title=request.json['title'],
-            content=request.json['content']
-        )
-        db.session.add(new_post)
+        data = request.json
+        post = Post(title=data['title'], content=data['content'])
+        db.session.add(post)
         db.session.commit()
-        return post_schema.dump(new_post)
-
+        return post_schema.dump(post)
 
 class PostResource(Resource):
-    def get(self, post_id):
-        post = Post.query.get_or_404(post_id)
-        return post_schema.dump(post)
+    def get(self, pk):
+        return post_schema.dump(Post.query.get_or_404(pk))
 
-    def patch(self, post_id):
-        post = Post.query.get_or_404(post_id)
+    def patch(self, pk):
+        data = request.json
+        post = Post.query.get_or_404(pk)
 
-        if 'title' in request.json:
-            post.title = request.json['title']
-        if 'content' in request.json:
-            post.content = request.json['content']
-
+        if 'title' in data:
+            post.title = data['title']
+        
+        if 'content' in data:
+            post.content = data['content']
+        
         db.session.commit()
         return post_schema.dump(post)
 
-    def delete(self, post_id):
-        post = Post.query.get_or_404(post_id)
+    def delete(self, pk):
+        post = Post.query.get_or_404(pk)
         db.session.delete(post)
         db.session.commit()
         return '', 204
 
-
-api.add_resource(PostListResource, '/posts')
-api.add_resource(PostResource, '/posts/<int:post_id>')
-
+api.add_resource(PostResource, '/post/<int:pk>')
+api.add_resource(PostsResource, '/posts')
 
 if __name__ == '__main__':
     app.run(debug=True)
